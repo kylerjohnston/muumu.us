f86e1683225ce86a4e7f9a9dd9355f6519000668
Kyle Johnston <kylerjohnston@gmail.com>
2020-11-25 17:15:44 -0500

    Test to ensure `tag1` template tag not used in production

--- _test/test_tags.rb
+++ _test/test_tags.rb
@@ -11,14 +11,23 @@ def find_posts(dir)
 end
 
 class TestTags < Minitest::Test
-  def test_all_posts_have_tags
+  def test_all_posts_have_tags_not_tag1
     year_dirs = Dir.children('_site').select { |f| f.start_with?('20') }
     year_dirs.each do |year_dir|
       posts = find_posts("_site/#{year_dir}")
       posts.each do |post|
         doc = Nokogiri::HTML.parse(open(post))
         tags = doc.css('a.post_tag')
+
+        # Make sure all posts have tags
         assert tags.length.positive?
+
+        tags.each do |tag|
+          tag.children.each do |child|
+            # Make sure the template tag `tag1` is not used
+            assert child.content.include?('tag1') == false
+          end
+        end
       end
     end
   end
